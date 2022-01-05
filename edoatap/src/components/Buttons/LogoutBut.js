b45306d39014b465543449d09d2dfb2b1a2a3a6d
Konstantinos <ktheofilis@gmail.com>
2022-01-06 01:07:20 +0200

    Minor fix

--- edoatap/src/components/Buttons/LogoutBut.js
+++ edoatap/src/components/Buttons/LogoutBut.js
@@ -10,7 +10,7 @@ export default function LogoutBut() {
     
     useEffect(()=>{
         setUsername(decodeToken().firstname)
-    })
+    }, [])
 
     function LogOut(){
         localStorage.removeItem('token')
